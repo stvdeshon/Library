5c2cfa14b08d5eb16c05cfb00e0a461fff56ba1a
stvdeshon <93623464+stvdeshon@users.noreply.github.com>
2022-05-06 11:24:48 -0600

    Save some provisional changes

--- library.js
+++ library.js
@@ -1,22 +1,28 @@
 
-//opens form to add new books
+//opens modal to add new books
 const addBook = document.querySelector("#new");
+//submits user input
+const submit = document.querySelector('#submit');
 //opens book entries
 const openBook = document.querySelectorAll(".open");
 //closes opened modals
 const closeModal = document.querySelectorAll(".close");
-//deletes selected books
-const removeBook = document.querySelectorAll(".remove");
+//deletes selected books (currently testing with this to see if id works rather than class on book objects)
+const removeBook = document.querySelector("#remove");
 //modal containers
 const formContainer = document.querySelector("#form-container");
 const bookContainer = document.querySelector("#book-container");
 //modals
 const formModal = document.querySelector("#form-modal");
 const bookModal = document.querySelector("#book-modal");
+//input values
 
 
 
-//button toggles
+
+//button events
+
+//modal toggles
 addBook.addEventListener('click', () => {
     formContainer.classList.add('show');
 });
@@ -27,7 +33,7 @@ openBook.forEach((book) => {
     })
 });
 
-//closes container divs with modals inside them
+//closes modals
 
 closeModal.forEach((modal) => {
     modal.addEventListener('click', () => {
@@ -36,20 +42,71 @@ closeModal.forEach((modal) => {
     })
 });
 
-//remove ONLY the selected book
-
-// removeBook.forEach((remove) => {
-//     remove.addEventListener('click', () => {
-        
-//     })
+//submits user info to constructBook function
+// submit.addEventListener('click', () => {
+//     console.log(constructBook('alice', 'carrol', 500, 'a bit'));
+//     formContainer.classList.remove('show');
 // });
+
+//parameter is filler, need to find how to feed specific index later
+removeBook.addEventListener('click', (index) => {
+    deleteBook(index);
+    bookContainer.classList.remove('show');
+});
 
 let myLibrary = [];
 
-function Book() {
 
+constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
 
+
+
+//submits user info to constructBook function
+submit.addEventListener('click', () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementsByName('read');
+    let radioVal;
+
+    for(i = 0; i < read.length; i++) {
+        if(read[i].checked)
+        radioVal = read[i].value;
+    }
+
+    
+    console.log(constructBook(title, author, pages, radioVal));
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    //can't remove the selection, might need to use checkbox
+    // document.getElementsByName('read').checked = false;
+    formContainer.classList.remove('show');
+});
+
+
+//constructs book, adds to array, will have to connect user input from form
+function constructBook(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    return newBook;
 }
+
+//deletes selected book from array, may have to add ui deletion later
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    return myLibrary;
+}
+
+
+
+
+
+
